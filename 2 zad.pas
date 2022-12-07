ee748f4e443e23bb5da7741036e00101928914a6
shameRRR <118363645+shameRRR@users.noreply.github.com>
2022-12-07 12:42:33 +0300

    Add files via upload

---
+++ 2 zad.pas
@@ -0,0 +1,23 @@
+﻿var str,s:string;
+arr:array [1..225] of string;
+l,n,i:byte;
+begin
+  readln(str);
+  l:=length(str);
+  n:=0;
+  s:='';
+   for i:=1 to l do
+    if ((str[i]=' ') or (i = l)) and (s<>'') then begin
+      n:= n + 1;
+      if (i=l) and (str[i] <> ' ') then s:= s + str[i];
+      arr[n]:=s;
+      s:= '';
+end
+else
+  if str[i] <> ' ' then
+    s:= s + str[i];
+writeln(n);
+for i:=1 to n do
+  writeln(arr[i],' - ',length(arr[i]));
+    
+end.
